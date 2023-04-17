93e2b703d0ce460cc546e65174e4a7ba6887ba37
Shadess <tylermboettcher@gmail.com>
2023-04-17 16:03:40 -0400

    Back to normal!

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import logo from "./assets/birdscreams.gif";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="text-center">
+      <header className="bg-slate-500 flex flex-col items-center justify-center min-h-screen text-white">
+        <img
+          src={logo}
+          className="animate-[spin_20s_linear_infinite] rounded-full text-xl w-60 z-10"
+          alt="logo"
+        />
+        <p
+          className="mt-2 text-2xl z-20"
+          style={{
+            textShadow:
+              "-1px 1px 0 #282c34, 1px 1px 0 #282c34, 1px -1px 0 #282c34",
+          }}
         >
-          Learn React
-        </a>
+          Birdscreams
+        </p>
       </header>
     </div>
   );
