bff6eddb13cf9c5ba4d759bdf01369e4fc28f827
Shadess <tylermboettcher@gmail.com>
2023-04-17 16:22:28 -0400

    Allowing named function components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,26 @@
 import logo from './assets/birdscreams.gif';
 
-const App = () => (
-  <div className="text-center">
-    <header className="bg-slate-500 flex flex-col items-center justify-center min-h-screen text-white">
-      <img
-        src={logo}
-        className="animate-[spin_20s_linear_infinite] rounded-full text-xl w-60 z-10"
-        alt="logo"
-      />
-      <p
-        className="mt-2 text-2xl z-20"
-        style={{
-          textShadow:
-            '-1px 1px 0 #282c34, 1px 1px 0 #282c34, 1px -1px 0 #282c34',
-        }}
-      >
-        Birdscreams
-      </p>
-    </header>
-  </div>
-);
+function App() {
+  return (
+    <div className="text-center">
+      <header className="bg-slate-500 flex flex-col items-center justify-center min-h-screen text-white">
+        <img
+          src={logo}
+          className="animate-[spin_20s_linear_infinite] rounded-full text-xl w-60 z-10"
+          alt="logo"
+        />
+        <p
+          className="mt-2 text-2xl z-20"
+          style={{
+            textShadow:
+              '-1px 1px 0 #282c34, 1px 1px 0 #282c34, 1px -1px 0 #282c34',
+          }}
+        >
+          Birdscreams
+        </p>
+      </header>
+    </div>
+  );
+}
 
 export default App;
